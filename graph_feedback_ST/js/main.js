9f9a15c66c51f38fd1c21e39100adf1737fb3995
nault22e <en27@hw.ac.uk>
2023-05-22 13:14:04 +0100

    Added all 3 graphs to one project graph_feedback_ST

--- graph_feedback_ST/js/main.js
+++ graph_feedback_ST/js/main.js
@@ -42,10 +42,10 @@ let default_web = new DefaultWeb();
 
 $(document).ready(function() {
 
-  google.charts.load('current', {'packages':['line', 'corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+    google.charts.load('current', {'packages':['line', 'corechart']});
+    google.charts.setOnLoadCallback(drawCMChart);
 
-    function drawChart() {
+    function drawSTChart() {
 
       var chartDiv = document.getElementById('chart_div');
 
@@ -55,6 +55,7 @@ $(document).ready(function() {
       data.addColumn('number', 'Errors');
       data.addColumn('number', 'Reaction Time');
 
+      //Attempt #, num correct, num errors, RT
       data.addRows([
         [1,  3, 0, 1.5],
         [2,  3, 1, 5],
@@ -81,7 +82,7 @@ $(document).ready(function() {
         vAxes: {
           // Adds titles to each axis.
           0: {title: 'Number Correct/Number of Errors'},
-          1: {title: 'Reaction Time (seconds)'}
+          1: {title: 'Reaction Time (secs)'}
           
         },
         hAxis: {
@@ -116,25 +117,178 @@ $(document).ready(function() {
 
       var classicChart = new google.visualization.LineChart(chartDiv);
       classicChart.draw(data, classicOptions);
-        
-      
+    }
 
+    function drawCCChart() {
 
+      var chartDiv = document.getElementById('chart_div');
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Attempts');
+      data.addColumn('number', 'Duration');
+      data.addColumn('number', 'Errors');
+      data.addColumn('number', 'Reaction Time');
+
+      //Attempt #, num correct, num errors, RT
+      data.addRows([
+        [1,  3, 1.6, 1.5],
+        [2,  0, 1.5, 1.8],
+        [3,  3, 3.1, 1.2],
+        [4,  1, 2.6, 1.4],
+        [5,  3, 2.4, 1.5]
+        // [6,  3, 1, 1.5]
+      ]);
+
+      var classicOptions = {
+        title: 'Category Checker Feedback',
+        width: 1100,
+        height: 600,
+
+        titleTextStyle: {
+          fontSize: 40
+        },
+        // Gives each series an axis that matches the vAxes number below.
+        series: {
+          0: {targetAxisIndex: 0},
+          1: {targetAxisIndex: 1},
+          2: {targetAxisIndex: 1}
+        },
+        vAxes: {
+          // Adds titles to each axis.
+          0: {title: 'Number of Errors'},
+          1: {title: 'Duration (mins) / \nReaction Time (secs)'}
+          
+        },
+        hAxis: {
+          textStyle : {
+            fontSize: 24 
+          },
+          titleTextStyle: {
+            fontSize: 24 
+          },
+          title: 'Attempts',
+          ticks: [1, 2, 3, 4, 5, 6]
+        },
+        vAxis: {
+          // viewWindow: {
+          //   max: 8
+          // },
+          textStyle : {
+            fontSize: 24 
+          },
+          titleTextStyle: {
+            fontSize: 24 
+          },
+        },
+
+        legend: {
+          textStyle: {
+            fontSize: 24
+          }
+        },
+        lineWidth: 7,
+      };
+
+      var classicChart = new google.visualization.LineChart(chartDiv);
+      classicChart.draw(data, classicOptions);
+    }
+
+    function drawCMChart() {
+
+      var chartDiv = document.getElementById('chart_div');
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Attempts');
+      data.addColumn('number', '# Pairs');
+      data.addColumn('number', 'Duration');
+      data.addColumn('number', 'Reaction Time');
+
+      //Attempt #, num correct, num errors, RT
+      data.addRows([
+        [1,  8, 0.8, 1.5],
+        [2,  6, 1.5, 1.5],
+        [3,  7, 0.9, 1.5],
+        [4,  8, 1.8, 1.5],
+        [5,  6, 1.3, 1.8]
+        // [6,  3, 1, 1.5]
+      ]);
+
+      var classicOptions = {
+        title: 'Card Matching Feedback',
+        width: 1100,
+        height: 600,
+
+        titleTextStyle: {
+          fontSize: 40
+        },
+        // Gives each series an axis that matches the vAxes number below.
+        series: {
+          0: {targetAxisIndex: 0},
+          1: {targetAxisIndex: 1},
+          2: {targetAxisIndex: 1}
+        },
+        vAxes: {
+          // Adds titles to each axis.
+          0: {title: 'Number of Pairs'},
+          1: {title: 'Duration (mins) / \nReaction Time (secs)'}
+          
+        },
+        hAxis: {
+          textStyle : {
+            fontSize: 24 
+          },
+          titleTextStyle: {
+            fontSize: 24 
+          },
+          title: 'Attempts',
+          ticks: [1, 2, 3, 4, 5, 6]
+        },
+        vAxis: {
+          // viewWindow: {
+          //   max: 8
+          // },
+          textStyle : {
+            fontSize: 24 
+          },
+          titleTextStyle: {
+            fontSize: 24 
+          }
+        },
+
+        legend: {
+          textStyle: {
+            fontSize: 24
+          }
+        },
+        lineWidth: 7,
+      };
+
+      var classicChart = new google.visualization.LineChart(chartDiv);
+      classicChart.draw(data, classicOptions);
     }
     
   // Add event listeners
     $("#cmbutton").on("touchend", function(){
-   // parent.switchConfig("memory_game");
-    window.open("../customisation_category/index.html", "_self");
+        google.charts.load('current', {'packages':['line', 'corechart']});
+        google.charts.setOnLoadCallback(drawCMChart);
+        document.getElementById("stbutton").className = "big-green-btn";
+        document.getElementById("cmbutton").className = "disabled-btn";
+        document.getElementById("ccbutton").className = "big-green-btn";
   });
     $("#ccbutton").on("touchend", function(){
-   // parent.switchConfig("memory_game");
-    window.open("../customisation_category/index.html", "_self");
+        google.charts.load('current', {'packages':['line', 'corechart']});
+        google.charts.setOnLoadCallback(drawCCChart);
+        document.getElementById("stbutton").className = "big-green-btn";
+        document.getElementById("cmbutton").className = "big-green-btn";
+        document.getElementById("ccbutton").className = "disabled-btn";
   });
     
     $("#stbutton").on("touchend", function(){
-   // parent.switchConfig("memory_game");
-    window.open("../customisation_category/index.html", "_self");
+        google.charts.load('current', {'packages':['line', 'corechart']});
+        google.charts.setOnLoadCallback(drawSTChart);
+        document.getElementById("stbutton").className = "disabled-btn";
+        document.getElementById("cmbutton").className = "big-green-btn";
+        document.getElementById("ccbutton").className = "big-green-btn";
   });
 });
 
